ddc2366eaa2dcd23cc5896fd7d4712dfc4d4f5a1
Mikx0125 <31879838+Mikx0125@users.noreply.github.com>
2018-09-17 11:10:47 +0200

    Initial commit

---
+++ MyMathDLLTest/MathTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyMathDLL;
+
+
+namespace MyMathDLLTest
+{
+    [TestClass]
+    public class MathTest
+    {
+        //sdkfkop
+        MyMath m = new MyMath();
+        [TestMethod]
+        public void AddTest()
+        {
+            
+            Assert.IsTrue(m.Add(2,3).Equals(5)); 
+        }
+        [TestMethod]
+        public void addTestNegativeNumber()
+        {
+            Assert.AreEqual(m.Add(-3, -4), -7);
+        }
+        [TestMethod]
+        public void MultipleTest()
+        {
+            Assert.AreEqual(m.Multiple(2, 4), 8);
+        }
+
+
+
+    }
+}
---
+++ MyMathDLLTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyMathDLLTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyMathDLLTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ed57a22c-9655-4dc5-b907-69db1538bff1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
